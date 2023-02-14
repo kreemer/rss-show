67dc24c1f47218618dc6308cd58533469d461e79
kreemer <kreemer@me.com>
2023-02-14 09:04:38 +0100

    Add more feeds

--- src/app/feed/mock-feeds.ts
+++ src/app/feed/mock-feeds.ts
@@ -3,5 +3,7 @@ import { Feed } from "../feed";
 export const FEEDS: Feed[] = [
     { title: 'SRF', url: 'https://www.srf.ch/news/bnf/rss/1646' },
     { title: 'NZZ', url: 'https://www.nzz.ch/recent.rss' },
-    { title: 'Bund', url: 'https://partner-feeds.publishing.tamedia.ch/rss/derbund/ticker' } 
+    { title: 'Bund', url: 'https://partner-feeds.publishing.tamedia.ch/rss/derbund/ticker' },
+    { title: 'xkcd', url: 'https://xkcd.com/rss.xml' }
+     
 ]
