914cba88afabae86e8898353605a60073e930a2f
kreemer <kreemer@me.com>
2023-02-14 10:21:02 +0100

    Add feed

--- src/app/feed/mock-feeds.ts
+++ src/app/feed/mock-feeds.ts
@@ -4,6 +4,7 @@ export const FEEDS: Feed[] = [
     { title: 'SRF', url: 'https://www.srf.ch/news/bnf/rss/1646', onlyImage: false },
     { title: 'NZZ', url: 'https://www.nzz.ch/recent.rss', onlyImage: false },
     { title: 'Bund', url: 'https://partner-feeds.publishing.tamedia.ch/rss/derbund/ticker', onlyImage: false },
-    { title: 'xkcd', url: 'https://xkcd.com/rss.xml', onlyImage: true }
+    { title: 'xkcd', url: 'https://xkcd.com/rss.xml', onlyImage: true },
+    { title: 'Postillon', url: 'https://follow.it/der-postillon-abo?user=kreemer&key=2k6O1LHNPrM', onlyImage: false}
      
 ]
